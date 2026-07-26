7f288ba0b7c251dc1ca43c56ebfa5c04181aa490
Sadaf <sadafmashorihyd@gmail.com>
2026-07-26 11:45:42 +0500

    fix DM layout

---
+++ app/dm/layout.jsx
@@ -0,0 +1,14 @@
+export default function DmLayout({ children }) {
+    return (
+        <div style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            overflow: 'hidden'
+        }}>
+            {children}
+        </div>
+    )
+}

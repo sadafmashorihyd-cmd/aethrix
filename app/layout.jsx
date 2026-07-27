2886ac4649552b25cf8c73adb542ce09b8e782e7
Sadaf <sadafmashorihyd@gmail.com>
2026-07-27 15:33:35 +0500

    mural font update

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,600&family=Playfair+Display:ital,wght@0,700;0,900;1,700;1,900&display=swap"
           rel="stylesheet"
         />
       </head>
